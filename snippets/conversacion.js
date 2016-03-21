e1d3e2c33867633f772e78064bc216871dafa59f
gaedef <luisgael.delacruz@gmail.com>
2016-03-21 13:29:43 -0600

    se agrega el ultimo programa

---
+++ snippets/conversacion.js
@@ -0,0 +1,21 @@
+var luisitoName = 'Luisito'
+var pirloName ='Pirlo'
+var luisitoAge ='12 años'
+var pirloAge ='43 años'
+var britaniaTeam = 'britania'
+var juventusTeam = 'Juventus'
+var luisitoGoals = '3'
+var pirloGoals = '0'
+var respuesta3 = 'hasta luego .'
+
+console.log('Hola ¿Como te llamas?')
+console.log('Hola yo me llamo ' + pirloName + ' ¿y tu?')
+console.log('Yo me llamo ' + luisitoName + ' ¿Cuantos años tienes?')
+console.log("Yo tengo " + pirloAge + " ¿y tu?")
+console.log('Yo tengo ' + luisitoAge + ' ¿En que equipo juegas?')
+console.log('en la ' + juventusTeam + ' ¿y tu?')
+console.log('en el ' + britaniaTeam + ' ¿Cuantos goles metiste en el ultimo partido?')
+console.log('Yo meti ' + pirloGoals + ' ¿Y tu?')
+console.log('yo meti ' + luisitoGoals)
+
+
