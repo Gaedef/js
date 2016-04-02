5bf20f10c142f5699f25527326488e383570047b
gaedef <luisgael.delacruz@gmail.com>
2016-04-01 18:20:36 -0600

    Refactorizando los nidos de if

--- snippets/fibonacci.js
+++ snippets/fibonacci.js
@@ -1,17 +1,7 @@
 var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 numbers.forEach(function(number){
-	if(number == 1){
+	if(number == 1 || number == 2 || number==3 || number == 5 || number==8 || number==13){
 	  console.log(number)
-	} else if(number == 2){
-      console.log(number)
-	} else if(number == 3){
-      console.log(number)
-	} else if(number == 5){
-      console.log(number)
-	} else if(number == 8){
-      console.log(number)
-	} else if(number == 13){
-      console.log(number)
-	}
+	} 
 })
