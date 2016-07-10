0f564c69118c96e3c11b5416fed97ae5c45dd5e1
gaedef <luisgael.delacruz@gmail.com>
2016-07-09 22:48:44 -0500

    subiendo ejemplos de meses

---
+++ snippets/months.js
@@ -0,0 +1,17 @@
+var month = 33
+
+if( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+  console.log(" tiene 31 dias ")
+}
+
+if( month == 2 ) {
+  console.log("tiene 28 ó 29 dias")
+}
+
+if( month == 4 || month == 6 || month == 9 || month == 11) {
+  console.log(" tiene 30 dias ")
+}
+
+if(month < 0  || month > 12) {
+	console.log("ese mes no existe ,pero puedes intentar con uno real ahora")
+}
